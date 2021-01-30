a91af6b608bd55b3463816c36b82644c69a5b03f
James Galantino <james_galantino@aol.com>
2021-01-30 00:21:21 -0500

    more styling and added click event to project1

---
+++ script.js
@@ -0,0 +1,9 @@
+// Project URLs
+var project1URL = 'https://jimbopulos.github.io/rec-app/'
+
+// Click functions for portfolio images
+var project = document.querySelector('#project1');
+
+project.addEventListener('click', function () {
+    window.open(project1URL);
+})
