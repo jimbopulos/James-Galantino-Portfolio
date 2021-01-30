4e115b0208c60ce446626761637b35a5c36caa9f
James Galantino <james_galantino@aol.com>
2021-01-30 00:23:27 -0500

    Linked script.js to portfolio.html

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var project1URL = 'https://jimbopulos.github.io/rec-app/'
 // Click functions for portfolio images
 var project = document.querySelector('#project1');
 
-project.addEventListener('click', function () {
+project.addEventListener('click', function() {
     window.open(project1URL);
+    console.log(project1URL)
 })
