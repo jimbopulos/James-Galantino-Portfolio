1a495a589b5af2b0c1d28255623174fda5b895aa
James Galantino <james_galantino@aol.com>
2021-01-30 00:29:56 -0500

    Added click events to rest of portfolio items

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 // Project URLs
-var project1URL = 'https://jimbopulos.github.io/rec-app/'
+var project1URL = 'https://jimbopulos.github.io/rec-app/';
+var plannerURL = 'https://jimbopulos.github.io/day-planner/';
+var passwordgenURL = 'https://jimbopulos.github.io/random-password-generator/';
 
 // Click functions for portfolio images
-var project = document.querySelector('#project1');
+var project = document.getElementById('project1');
 
 project.addEventListener('click', function() {
     window.open(project1URL);
-    console.log(project1URL)
+})
+
+var dayPlanner = document.getElementById('day-planner')
+
+dayPlanner.addEventListener('click', function() {
+    window.open(plannerURL);
+})
+
+var passwordgenURL = document.getElementById('password-gen')
+
+passwordgenURL.addEventListener('click', function() {
+    window.open(passwordgenURL);
 })
