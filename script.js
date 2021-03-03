16ae6748035d02473c2f79c8b4eb50281378143f
James Galantino <james_galantino@aol.com>
2021-03-02 19:22:07 -0500

    Added two new projects to portfolio, added func for links

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 // Project URLs
 var project1URL = 'https://jimbopulos.github.io/rec-app/';
+var project2URL = 'https://four-top.herokuapp.com/';
 var plannerURL = 'https://jimbopulos.github.io/day-planner/';
-var passwordgenURL = 'https://jimbopulos.github.io/random-password-generator/';
+var burgerURL = 'https://eat-burgerlist-app.herokuapp.com/';
+// var passwordgenURL = 'https://jimbopulos.github.io/random-password-generator/';
 
 // Click functions for portfolio images
 var project = document.getElementById('project1');
 
 project.addEventListener('click', function() {
     window.open(project1URL);
-})
+});
+
+var project2 = document.getElementById('project2');
+
+project2.addEventListener('click', function() {
+    window.open(project2URL);
+});
 
 var dayPlanner = document.getElementById('day-planner')
 
 dayPlanner.addEventListener('click', function() {
     window.open(plannerURL);
-})
+});
 
-var passwordGen = document.getElementById('password-gen')
+var burger = document.getElementById('eat-da-burger')
 
-passwordGen.addEventListener('click', function() {
-    window.open(passwordgenURL);
-})
+burger.addEventListener('click', function() {
+    window.open(burgerURL);
+});
+
+// var passwordGen = document.getElementById('password-gen')
+
+// passwordGen.addEventListener('click', function() {
+//     window.open(passwordgenURL);
+// });
